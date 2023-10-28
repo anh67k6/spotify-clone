4bd103e53f6cdbd42b793a4d3daac92e5a3593eb
anh67k6 <dohoanganh16032002@gmail.com>
2023-10-28 20:33:41 +0700

    f-1 add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-        Hiii
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<div>Hello</div>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
