50367b51f9e5bdeaac1919e87f6ca40b922583fb
quoctrungict01 <quoctrung.3hd@gmail.com>
2023-10-30 21:15:05 +0700

    login part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
+import './output.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import LoginComponent from './routes/Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-screen h-screen">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<div>Hello</div>} />
+          <Route path='/' element={<HelloComponent />} />
+          <Route path='/login' element={<LoginComponent />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
+const HelloComponent = () => {
+  return <div>This is Hello from component</div>;
+};
 
 export default App;
---
+++ frontend/src/routes/Login.js
@@ -0,0 +1,15 @@
+import { Icon } from "@iconify/react"
+const LoginComponent = () => {
+    return (
+        <div className="w-full h-full flex flex-col items-center">
+            <div className="logo p-5 border border-solid border-gray-300 w-full flex justify-center">
+                <Icon icon="logos:spotify" width="150" />
+            </div>
+            <div className="inputRegion">
+
+            </div>
+        </div>
+    );
+};
+
+export default LoginComponent;
