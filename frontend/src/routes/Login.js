6457b494e36f0a3d5728b663fb63420a1ed4bb8a
Hung Lai <hung.lt205155@sis.hust.edu.vn>
2023-11-02 00:45:42 +0700

    Add PasswordInput and TextInput

--- frontend/src/routes/Login.js
+++ frontend/src/routes/Login.js
@@ -1,15 +1,25 @@
-import { Icon } from "@iconify/react"
+import { Icon } from "@iconify/react";
+import TextInput from "../components/shared/TextInput";
+import PasswordInput from "../components/shared/PasswordInput";
 const LoginComponent = () => {
-    return (
-        <div className="w-full h-full flex flex-col items-center">
-            <div className="logo p-5 border border-solid border-gray-300 w-full flex justify-center">
-                <Icon icon="logos:spotify" width="150" />
-            </div>
-            <div className="inputRegion">
-
-            </div>
-        </div>
-    );
+  return (
+    <div className="w-full h-full flex flex-col items-center">
+      <div className="logo p-5 border border-solid border-gray-300 w-full flex justify-center">
+        <Icon icon="logos:spotify" width="150" />
+      </div>
+      <div className="inputRegion w-1/3 py-10 flex items-center justify-center flex-col">
+        <div className="font-bold mb-12">To continue, log in to Spotify.</div>
+        <TextInput
+          label="Email address or username"
+          placeholder="Email address or username"
+        ></TextInput>
+        <PasswordInput
+          label="Password"
+          placeholder="Password"
+        ></PasswordInput>
+      </div>
+    </div>
+  );
 };
 
 export default LoginComponent;
