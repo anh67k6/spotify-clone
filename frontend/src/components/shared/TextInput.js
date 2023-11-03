90937434ca4403a57dd0accdd6b70a229942b5a0
Spawn021 <tranquangnam27012002@gmail.com>
2023-11-04 02:11:41 +0700

    add login part 3

--- frontend/src/components/shared/TextInput.js
+++ frontend/src/components/shared/TextInput.js
@@ -1,6 +1,6 @@
-const TextInput = ({label,placeholder}) => {
+const TextInput = ({ label, placeholder, className }) => {
   return (
-    <div className="textInputDiv flex flex-col space-y-2 w-full">
+    <div className={`textInputDiv flex flex-col space-y-2 w-full ${className}`}>
       <label for={label} className="font-semibold">
         {label}
       </label>
