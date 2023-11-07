def58a39aa65273fed5c88d07c9a79c1680523b0
quoctrungict01 <quoctrung.3hd@gmail.com>
2023-11-07 11:08:21 +0700

    Signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import './output.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import LoginComponent from './routes/Login';
+import SignupComponent from './routes/Signup';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HelloComponent />} />
           <Route path='/login' element={<LoginComponent />} />
+          <Route path='/signup' element={<SignupComponent />} />
         </Routes>
       </BrowserRouter>
     </div>
