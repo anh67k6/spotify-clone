bfd2eddd620d2b51fc3393b08e8821ee07bb97d8
Tong Tran Minh Duc <duc.ttm205147@sis.hust.edu>
2023-11-09 16:23:55 +0700

    back9

--- backend/models/Playlist.js
+++ backend/models/Playlist.js
@@ -1,32 +1,32 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Playlist = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+  },
+  songs: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'Song',
     },
-    thumbnail: {
-        type: String,
-        required: true,
+  ],
+  collaborators: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
     },
-    owner: {
-        type: mongoose.Types.ObjectId,
-        ref: "User",
-    },
-    songs: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: "Song",
-        },
-    ],
-    collaborators: [
-        {
-            type: mongoose.Types.ObjectId,
-            ref: "User",
-        },
-    ],
+  ],
 });
 
-const PlaylistModel = mongoose.model("Playlist", Playlist);
+const PlaylistModel = mongoose.model('Playlist', Playlist);
 
 module.exports = PlaylistModel;
