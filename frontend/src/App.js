2574255cc829a8ee1ce0339c78a38a64560976f4
Quangkubo209 <Quang.nn205167@sis.hust.edu.vn>
2023-12-29 21:28:00 +0700

    add context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import "./output.css";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginComponent from "./routes/Login";
 import SignupComponent from "./routes/Signup";
@@ -8,18 +9,27 @@ import { useCookies } from "react-cookie";
 import { Navigate } from "react-router-dom";
 import LoggedInHomeComponent from "./routes/LoggedInHome";
 import UploadSong from "./routes/UploadSong";
+import MyMusic from "./routes/MyMusic";
+import songContext from "./contexts/songContext";
+
 function App() {
+  const [currentSong, setCurrentSong] = useState(null);
   const [cookie, setCookie] = useCookies(["token"]);
+
   return (
     <div className="w-screen h-screen font-poppins">
       <BrowserRouter>
         {cookie.token ? (
           <Routes>
-            <Route path="/" element={<HelloComponent />} />
-            <Route path="/home" element={<LoggedInHomeComponent />} />
-            <Route path="/uploadSong" element={<UploadSong />} />
-            <Route path="*" element={<Navigate to="/home" />} />
+            <songContext.Provider value={{currentSong, setCurrentSong}}>
+              <Route path="/" element={<HelloComponent />} />
+              <Route path="/home" element={<LoggedInHomeComponent />} />
+              <Route path="/uploadSong" element={<UploadSong />} />
+              <Route path="/myMusic" element={<MyMusic />} />
+              <Route path="*" element={<Navigate to="/home" />} />
+              </songContext.Provider >
           </Routes>
+
         ) : (
           <Routes>
             <Route path="/home" element={<HomeComponent />} />
