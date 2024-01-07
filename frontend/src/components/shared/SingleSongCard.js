5db2b08a74474ba17507686cad375bede03e454d
Hoang AnH <dohoanganh16032002@gmail.com>
2024-01-07 21:32:50 +0700

    Fix time song error

--- frontend/src/components/shared/SingleSongCard.js
+++ frontend/src/components/shared/SingleSongCard.js
@@ -3,7 +3,14 @@ import songContext from "../../contexts/songContext";
 
 const SingleSongCard = ({ info, playSound }) => {
     const { currentSong, setCurrentSong } = useContext(songContext);
-
+    console.log(info)
+    function secondsToMinutes(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = Math.round(seconds % 60);
+        const formattedTime = `${minutes}:${remainingSeconds < 10 ? '0' : ''}${remainingSeconds}`;
+        return formattedTime;
+      }
+      
     return (
         <div
             className="flex hover:bg-gray-400 hover:bg-opacity-20 p-2 rounded-sm"
@@ -25,7 +32,7 @@ const SingleSongCard = ({ info, playSound }) => {
                     </div>
                 </div>
                 <div className="w-1/6 flex items-center justify-center text-gray-400 text-sm">
-                    <div>3:44</div>
+                    <div>{secondsToMinutes(info.duration)}</div>
                 </div>
             </div>
         </div>
